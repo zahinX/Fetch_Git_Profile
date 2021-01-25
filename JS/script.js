a00805e6d3364077c5e7d6d2c21a28d42f728b44
zahinX <77698979+zahinX@users.noreply.github.com>
2021-01-26 04:03:13 +0600

    added functionality for showing user data

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,22 @@
-let searchBtn= document.querySelector('#searchBtn');
-let searchUser= document.querySelector('#searchUser');
+let searchBtn = document.querySelector('#searchBtn');
+let searchUser = document.querySelector('#searchUser');
+let user = new UserProfile();
 
 searchBtn.addEventListener('click', (e) => {
-    let userText= searchUser.value;
-    if(userText!=''){
+    let userText = searchUser.value;
+    if (userText != '') {
         //Using Fetch API
         fetch('https://api.github.com/users/${userText}')
-        .then(result => result.json())
-        .then(data => {
-            if(data.message == 'Not Found'){
-                //show alert
-            }else{
-                //show profile
-            }
-        })
-    }else{
+            .then(result => result.json())
+            .then(data => {
+                if (data.message == 'Not Found') {
+                    //show alert
+                } else {
+                    //show profile
+                    user.showProfile(data);
+                }
+            })
+    } else {
         //clear search bar
     }
 });
