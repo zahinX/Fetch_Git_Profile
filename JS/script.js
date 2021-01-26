db51727d9aad227de1dc4de7d4b96438dcfb3c67
zahinX <77698979+zahinX@users.noreply.github.com>
2021-01-26 21:08:37 +0600

    added alert and clear profile function

--- JS/script.js
+++ JS/script.js
@@ -6,17 +6,21 @@ searchBtn.addEventListener('click', (e) => {
     let userText = searchUser.value;
     if (userText != '') {
         //Using Fetch API
-        fetch('https://api.github.com/users/${userText}')
+        fetch(`https://api.github.com/users/${userText}`)
             .then(result => result.json())
             .then(data => {
                 if (data.message == 'Not Found') {
                     //show alert
+                    user.showAlert("User not found!","alert alert-danger");
                 } else {
                     //show profile
                     user.showProfile(data);
                 }
             })
     } else {
-        //clear search bar
+        //clear fetched profile
+        user.clearProfile();
     }
 });
+
+//fetch('https://api.github.com/users/${userText}/repos');
